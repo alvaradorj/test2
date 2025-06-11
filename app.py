ea7fa5dc5504498bbbd3ea95d593c639d5534636
alvaradorj <alvarado.rj@gmail.com>
2025-06-11 18:28:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import pdfplumber
 import io
+import base64
 
-st.title("Canvas TEST con imagen PDF de fondo")
+st.title("Canvas TEST con imagen base64")
 
 uploaded_file = st.file_uploader("Sube tu PDF", type="pdf")
 if uploaded_file:
@@ -12,8 +13,15 @@ if uploaded_file:
         page = pdf.pages[0]
         img = page.to_image(resolution=150).original.convert("RGB")
         st.image(img, caption="Previsualización directa", use_container_width=True)
-        st.write(type(img), img.mode, img.size)
         width, height = img.size
+
+    def pil_to_base64(img):
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        base64_img = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        return f"data:image/png;base64,{base64_img}"
+
+    img_base64 = pil_to_base64(img)
 
     st.markdown("Canvas visual con imagen PDF")
     canvas_result = st_canvas(
@@ -21,7 +29,7 @@ if uploaded_file:
         stroke_width=2,
         stroke_color="#ff8800",
         background_color="#fff",
-        background_image=img,  # <--- DIRECTAMENTE el objeto PIL.Image
+        background_image_url=img_base64,
         update_streamlit=True,
         height=height,
         width=width,
